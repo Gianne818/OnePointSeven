3950a0d511057fcf9cd2b8b3138e0acea7283152
Gianne <cruzgian008@gmail.com>
2025-07-06 22:32:14 +0800

    completed

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ proceedButton.addEventListener("click", event =>{
     }, 500);
 
 });
+
+let FM = document.getElementById("FM");
+let CM = document.getElementById("CM");
+let Dm = document.getElementById("Dm");
+let BbM = document.getElementById("BbM");
+
+let uod = document.getElementById("uod");
+let uodMusic = document.getElementById("uodMusic");
+
+let num = 1;
+
+mainContent.addEventListener("click", event =>{
+    if(num === 5) num = 1;
+    switch(num){
+        case 1: 
+            FM.play();
+            num++;
+            break;
+        case 2: 
+            CM.play();
+            num++;
+            break;
+        case 3: 
+            Dm.play();
+            num++;
+            break;
+        case 4: 
+            BbM.play();
+            num++;
+            break;
+    }
+});
+
+uod.addEventListener("click", event =>{
+    uodMusic.play();
+})
